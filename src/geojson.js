47474dc3daf8a8a9a20df8d63856bcb9b26a29b5
e.sodacci <e.sodacci@agricolus.com>
2022-08-11 15:50:07 +0200

    change dependency to a sane dbf package test with multi rings polygons

--- src/geojson.js
+++ src/geojson.js
@@ -3,30 +3,35 @@ module.exports.line = justType('LineString', 'POLYLINE');
 module.exports.polygon = justType('Polygon', 'POLYGON');
 
 function justType(type, TYPE) {
-    return function(gj) {
-        var oftype = gj.features.filter(isType(type));
-        return {
-            geometries: (TYPE === 'POLYGON' || TYPE === 'POLYLINE') ? [oftype.map(justCoords)] : oftype.map(justCoords),
-            properties: oftype.map(justProps),
-            type: TYPE
-        };
-    };
+	return function (gj) {
+		var oftype = gj.features.filter(isType(type));
+		return {
+			geometries: (TYPE === 'POLYGON' || TYPE === 'POLYLINE') ? [oftype.map(justCoords)] : oftype.map(justCoords),
+			properties: oftype.map(justProps),
+			type: TYPE
+		};
+	};
 }
 
 function justCoords(t) {
-    if (t.geometry.coordinates[0] !== undefined &&
-        t.geometry.coordinates[0][0] !== undefined &&
-        t.geometry.coordinates[0][0][0] !== undefined) {
-        return t.geometry.coordinates[0];
-    } else {
-        return t.geometry.coordinates;
-    }
+	if (t.geometry.coordinates[0] !== undefined &&
+		t.geometry.coordinates[0][0] !== undefined &&
+		t.geometry.coordinates[0][0][0] !== undefined) {
+		if (t.geometry.coordinates[0].length > 1) {
+			t.geometries.coordinates
+		}
+		else {
+			return t.geometry.coordinates[0];
+		}
+	} else {
+		return t.geometry.coordinates;
+	}
 }
 
 function justProps(t) {
-    return t.properties;
+	return t.properties;
 }
 
 function isType(t) {
-    return function(f) { return f.geometry.type === t; };
+	return function (f) { return f.geometry.type === t; };
 }
